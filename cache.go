deed6e7e26dc7908f44ba2f45a28095f33484f2e
serg-sg <serg_eronov@live.ru>
2022-06-11 16:59:49 +0400

    Done

--- cache.go
+++ cache.go
@@ -3,21 +3,74 @@ package cache
 import "time"
 
 type Cache struct {
+	items map[string]struct {
+		key      string
+		value    string
+		deadline time.Time
+	}
 }
 
 func NewCache() Cache {
-	return Cache{}
+	i := make(map[string]struct {
+		key      string
+		value    string
+		deadline time.Time
+	})
+	return Cache{items: i}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (a *Cache) Get(key string) (string, bool) {
+	var b string
+	var ok bool
+	i, found := a.items[key]
+	if !found {
+		return "", false
+	}
+	if i.deadline.IsZero() {
+		b = i.value
+		ok = true
+	} else {
+		if i.deadline.Before(time.Now()) {
+			delete(a.items, key)
+			b = ""
+			ok = false
+		} else {
+			b = i.value
+			ok = true
+		}
+	}
+	return b, ok
 }
 
-func (receiver) Put(key, value string) {
+func (a *Cache) Put(key, value string) {
+	type item struct {
+		key, value string
+		deadline   time.Time
+	}
+	a.items[key] = item{
+		value: value,
+	}
 }
 
-func (receiver) Keys() []string {
+func (a *Cache) Keys() []string {
+	var v []string
+	for i := range a.items {
+		_, ok := a.Get(i)
+		if !ok {
+			continue
+		}
+		v = append(v, i)
+	}
+	return v
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (a *Cache) PutTill(key, value string, deadline time.Time) {
+	type item struct {
+		key, value string
+		deadline   time.Time
+	}
+	a.items[key] = item{
+		value:    value,
+		deadline: deadline,
+	}
 }
